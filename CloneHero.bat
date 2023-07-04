c04dd9c4cf9a229403ab8916e207fcc1045913fd
hpalmen <mosixx@gmail.com>
2023-07-04 11:10:52 +0200

    scores update

--- CloneHero.bat
+++ CloneHero.bat
@@ -1,5 +1,16 @@
+@echo off
 git pull -v
-"Clone Hero.exe" 
+
+rem Check if the file exists
+if exist "C:\scripts\drumstation_obs_websocket_client.py" (
+    echo File 'drumstation_obs_websocket_client.py' found. Executing Python script...
+    py "C:\scripts\drumstation_obs_websocket_client.py"
+) else (
+    echo File 'drumstation_obs_websocket_client.py' not found. Skipping Python script execution.
+)
+
+"Clone Hero.exe"
+
 git commit -a -m "scores update"
 git push -v
 pause
