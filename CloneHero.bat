b9a3315b0f7e10011ece03dab11828f372bf5626
hpalmen <mosixx@gmail.com>
2023-07-04 11:22:10 +0200

    script changed for drumstation scene swither automation

--- CloneHero.bat
+++ CloneHero.bat
@@ -4,7 +4,7 @@ git pull -v
 rem Check if the file exists
 if exist "C:\scripts\drumstation_obs_websocket_client.py" (
     echo File 'drumstation_obs_websocket_client.py' found. Executing Python script...
-    py "C:\scripts\drumstation_obs_websocket_client.py"
+    powershell -Command "Start-Process -FilePath 'py' -ArgumentList 'C:\scripts\drumstation_obs_websocket_client.py' -NoNewWindow"
 ) else (
     echo File 'drumstation_obs_websocket_client.py' not found. Skipping Python script execution.
 )
